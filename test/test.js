fa579d0473593d35c9e0e9d7ff9618cc128d36b7
Manuel Martins <manuelmartins25@gmail.com>
2015-09-18 19:32:15 +0000

    added test spec

---
+++ test/test.js
@@ -0,0 +1,40 @@
+/**
+ * (C) Copyright 2014 WebGAP (http://www.webgap.eu/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by: ManuelMartins
+ * Created on: 23-05-2014
+ *
+ */
+
+var JSONDBFS = require('../index'),
+    assert = require('assert');
+
+describe('JSONDBFS Driver', function testSpec() {
+
+  before(function (done) {
+    done();
+  });
+  
+  it('should create a new collection', function test(done) {
+    assert(false, true);
+    done();
+  });
+
+  it('should insert a new object', function test(done) {
+    assert(false, true);
+    done();
+  });
+
+});
